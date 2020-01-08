edd1b96f65318189146d41f73acf765342a8891c
David <david.yenicelik@gmail.com>
2020-01-08 16:59:06 +0100

    Added merging back into cluster label

--- src/models/cluster/chinesewhispers.py
+++ src/models/cluster/chinesewhispers.py
@@ -4,7 +4,10 @@
         [1] L 2 F/INESC-ID at SemEval-2019 Task 2: Unsupervised Lexical Semantic Frame Induction using Contextualized Word Representations
 
 """
+from collections import Counter
+
 import numpy as np
+import matplotlib.pyplot as plt
 
 from ax import ParameterType, RangeParameter, SearchSpace
 import networkx as nx
@@ -19,7 +22,39 @@ from src.models.cluster.base import BaseCluster
 class ChineseWhispers(BaseCluster):
     """
         Open parameters are:
+
+        For testing purposes, we use random embeddings.
+        We assume that BERT generate also random-like indecies
+        (as they cannot be clustered properly..)
     """
+    # def insert_hubs_back(self, X, cluster_labels):
+    #     for i in range(X.shape[0]):
+
+
+    def merge_unclustered_points_to_closest_cluster(self, cos, cluster_labels):
+        counter = Counter(cluster_labels)
+        # remove these things
+        free_clusters = set([x[0] for x in Counter(el for el in counter.elements() if counter[el] < self.min_cluster_size).items()])
+
+        # Check for closest item, which is in an acceptable cluster ...
+        for idx in range(len(cluster_labels)):
+            if cluster_labels[idx] in free_clusters:
+                # Find closest cluster
+                closest_elements = np.argsort(cos[idx, :])[::-1]
+
+                for element in closest_elements:
+                    if element not in free_clusters:
+                        closest_element = element
+                        break
+
+                # if no closest element is found, quit with an error emssage ...
+
+                print("Closest element is: ", closest_element)
+                cluster_labels[idx] = cluster_labels[closest_element]
+
+        return cluster_labels
+
+
 
     def _create_adjacency_matrix(self, X):
         """
@@ -29,28 +64,20 @@ class ChineseWhispers(BaseCluster):
         :return:
         """
 
-        def _cutoff_function_1(matr):
-            return np.mean(matr) - self.std_multiplier_ * np.std(X)
-
-        def _cutoff_function_2(matr):
-            return (np.mean(matr) + np.std(X)) / 2.
-
-        # TODO: Use a hyperparameter which selects the cutoff function ...
-        _cutoff_function = _cutoff_function_1 if self.std_cutoff else _cutoff_function_2
 
         cos = cosine_similarity(X, X)
+        print("Cos shape is: ", cos.shape)
 
-        # TODO: Use a hyperparameter to define whether we should take out identity elements ...
+        lower_cutoff_value = np.mean(cos) + self.std_multiplier_ * np.std(cos)
         # if self.remove_identity:
         #     cor[np.nonzero(np.identity(cor.shape[0]))] = 0.  # Was previously below the cutoff calculation..
+        cos[cos < lower_cutoff_value] = 0.
 
-        cos[cos > _cutoff_function(cos)] = 0.
-
-        # TODO: Use a hyperparameter to define wheter we should take out hubs or not
+        # # TODO: Use a hyperparameter to define wheter we should take out hubs or not
         summed_weights = np.sum(cos, axis=1)
-        print("Summed weights are", summed_weights)
+        # print("Summed weights are", summed_weights)
         self.hubs_ = np.argsort(summed_weights)[-self.remove_hub_number_:]
-        print("Summed weights are", summed_weights[self.hubs_])
+        # print("Summed weights are", summed_weights[self.hubs_])
 
         self.hubs_ = set(self.hubs_)
         self.hub_mask_ = [x for x in np.arange(cos.shape[0]) if x not in self.hubs_]
@@ -59,25 +86,48 @@ class ChineseWhispers(BaseCluster):
 
         # Shall I plot the chinese whispers just for the lulzz?
         cos[np.nonzero(np.identity(cos.shape[0]))] = 0.
+        # print("Final cos is: ", cos)
 
         # Now run the chinese whispers algorithm
         graph = nx.to_networkx_graph(cos, create_using=nx.DiGraph)
+        # graph = graph.convert_to_undirected()
+        nx.draw(graph, node_size=10)
+        plt.show()
 
-        chinese_whispers(graph, iterations=30) # iterations might depend on the number of clusters...
+        chinese_whispers(graph, seed=1337) # iterations might depend on the number of clusters...
 
         print("Clustered items are: ")
         out = list(sorted(aggregate_clusters(graph).items(), key=lambda e: len(e[1]), reverse=True))
         self.cluster_ = [x[0] for x in out]
+        self.cluster_ = self.merge_unclustered_points_to_closest_cluster(cos, self.cluster_)
+        
+        self.cluster_ = np.asarray(self.cluster_)
+
+        print("Self cluster is: ")
+        print(self.cluster_)
+        print(len(self.cluster_))
+
+        if self.verbose:
+            colors = [1. / graph.nodes[node]['label'] for node in graph.nodes()]
+            nx.draw_networkx(
+                graph,
+                node_color=colors,
+                node_size=10,
+                with_labels=False
+            )  # font_color='white', # cmap=plt.get_cmap('jet'),
+            plt.show()
 
 
-    def __init__(self, std_multiplier=1.9, remove_hub_number=50, std_cutoff=False):
+    def __init__(self, std_multiplier=2., remove_hub_number=100, min_cluster_size=5, verbose=False):
         super(ChineseWhispers, self).__init__()
         # metric is one of:
+
+        self.verbose = verbose
 
         # hyperparameters
         self.std_multiplier_ = std_multiplier
         self.remove_hub_number_ = remove_hub_number
-        self.std_cutoff = std_cutoff
+        self.min_cluster_size = min_cluster_size
 
     @classmethod
     def hyperparameter_dictionary(cls):
@@ -86,11 +136,6 @@ class ChineseWhispers(BaseCluster):
                 "name": "std_multiplier",
                 "type": "range",
                 "values": [(2 ** x) for x in range(1, 5)],
-            },
-            {
-                "name": "divided_cutoff",
-                "type": "choice",
-                "bounds": [False, True]
             },
             {
                 "name": "remove_hub_number",
@@ -103,3 +148,11 @@ class ChineseWhispers(BaseCluster):
         self._create_adjacency_matrix(X)
         return self.cluster_
 
+
+if __name__ == "__main__":
+    print("Check chinese whispers incl. plotting ..")
+    X = np.random.random((500, 150))
+
+    model = ChineseWhispers(verbose=True)
+
+    model.fit_predict(X)
