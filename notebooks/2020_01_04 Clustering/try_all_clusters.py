77f65ca9d6edb87637279f66a0d37a6176808302
David <david.yenicelik@gmail.com>
2020-01-11 14:52:31 +0100

    Added a detailed stacktrace

--- notebooks/2020_01_04 Clustering/try_all_clusters.py
+++ notebooks/2020_01_04 Clustering/try_all_clusters.py
@@ -11,11 +11,12 @@
     this is really as good as any other metric
 """
 
-from ax import optimize, SearchSpace
+import traceback
 import numpy as np
 from sklearn.decomposition import PCA
 from sklearn.metrics import adjusted_rand_score
 from sklearn.preprocessing import StandardScaler
+from ax import optimize, SearchSpace
 
 from src.config import args
 from src.embedding_generators.bert_embeddings import BertEmbedding
@@ -57,7 +58,19 @@ def _evaluate_model(model_class, arg, crossvalidation_data):
         )
         pred_clustering = model_class(arg).fit_predict(X)
         # Drop all indices that are unknown
-        pred_clustering = pred_clustering[known_indices]
+        try:
+            pred_clustering = pred_clustering[known_indices]
+        except Exception as e:
+            # Apparently, here is the point of most error.
+            # Will just try-catch heree until we find a better solution ...
+            print("Logging all variables")
+            print("Pred clustering is: ", pred_clustering)
+            print("Known indices are: ", known_indices)
+            print("Number of senses", number_of_senses, X.shape)
+            print("True clustering is: ", true_clustering)
+            print("Arguments are: ", arg)
+            # Finally fail again for the stacktrace to appear in the outer loop
+            pred_clustering = pred_clustering[known_indices]
 
         if len(np.unique(pred_clustering)) == 1:
             print("Couldn't find cluster!", np.unique(pred_clustering))
@@ -207,15 +220,20 @@ if __name__ == "__main__":
 
 
         def _current_eval_fun(p):
-            return _evaluate_model(
-                model_class=model_class,
-                arg=p,
-                crossvalidation_data=devset
-            )
+            try:
+                return _evaluate_model(
+                    model_class=model_class,
+                    arg=p,
+                    crossvalidation_data=devset
+                )
+            except Exception as e:
+                print("Error occurred!")
+                print(e)
+                traceback.print_tb(e.__traceback__)
+                return 0.
 
 
         try:
-
             best_parameters, best_values, experiment, model = optimize(
                 parameters=params,
                 evaluation_function=_current_eval_fun,
@@ -228,5 +246,6 @@ if __name__ == "__main__":
 
         except Exception as e:
             print("AGHHH")
+            traceback.print_tb(e.__traceback__)
             print(e)
             print("\n\n\n\n")
